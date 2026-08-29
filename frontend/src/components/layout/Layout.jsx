a02f76994e1d5ec9326d8114b47ef8bd76360c74
Gastón <gaston.lesende@gmail.com>
2026-08-29 21:00:32 +0200

    add engadgement banner

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import InstallBanner from '../common/InstallBanner';
 
 export default function Layout({ children }) {
   return (
@@ -7,6 +8,7 @@ export default function Layout({ children }) {
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
+      <InstallBanner />
     </div>
   );
 }
